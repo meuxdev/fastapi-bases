300c693d404801840c9e6cc533caabfb10b93235
Alejandro Andrade <meuxdev@gmail.com>
2022-06-13 00:51:53 -0500

    request body and response body

--- main.py
+++ main.py
@@ -1,9 +1,38 @@
-from fastapi import FastAPI
+# Python Stuff
+from typing import Optional
+# Pydantic
+from pydantic import BaseModel
+# FastAPI
+from fastapi import FastAPI, Body
+
 
 app = FastAPI()
 
 
-# path operation decoration
+# Models
+
+
+class Person(BaseModel):
+    first_name: str
+    last_name: str
+    age: int
+    # Optional Values
+    hair_color: Optional[str] = None
+    is_married: Optional[bool] = None
+
+
 @app.get("/")
 def home():
+    """
+        / -> branch
+        returns hello world JSON
+    """
     return {"Hello": "World"}
+
+
+# Request and Response Body
+# Body(...) -> obligatory body parameter
+@app.post("/person/new")
+def create_person(person: Person = Body(...)):
+    """/person/new -> creates a new person"""
+    return person
