f6825bdd9a2cfa61a458bf6b825158e301c6ec57
Alejandro Andrade <meuxdev@gmail.com>
2022-06-13 01:12:54 -0500

    Validations: Query parameters | Part1

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from typing import Optional
 # Pydantic
 from pydantic import BaseModel
 # FastAPI
-from fastapi import FastAPI, Body
+from fastapi import FastAPI, Body, Query
 
 
 app = FastAPI()
@@ -36,3 +36,16 @@ def home():
 def create_person(person: Person = Body(...)):
     """/person/new -> creates a new person"""
     return person
+
+
+# Validations querry params
+@app.get("/person/detail")
+def show_person(
+    # Validations and definitions of the query parameters
+    # query parameters should be optional
+    # path parameters always should be obligatory
+    name: Optional[str] = Query(None, min_length=1, max_length=50),
+    # ... -> meaning obligatory
+    age: str = Query(...)
+):
+    return {name: age}
